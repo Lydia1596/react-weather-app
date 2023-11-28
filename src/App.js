0e9c6adc68b0cc3c858c4ce4fcd75a632b7b2a91
Lydia1596 <lydiaprentice@hotmail.co.uk>
2023-11-28 19:58:31 +0000

    Remove container div

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="Container">
-        <header className="App-header">Weather App</header>
-        <footer>
-          This app is coded by Lydia Prentice and is{" "}
-          <a
-            href="https://github.com/Lydia1596/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </footer>
-      </div>
+      <header className="App-header">Weather App</header>
+      <footer>
+        This app is coded by Lydia Prentice and is{" "}
+        <a
+          href="https://github.com/Lydia1596/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
