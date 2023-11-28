9f1ee13c4b79a07922a62114e2a83abba3389898
Lydia1596 <lydiaprentice@hotmail.co.uk>
2023-11-28 19:56:16 +0000

    Install Bootstrap and add link to GitHub code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Weather App</header>
+      <div className="Container">
+        <header className="App-header">Weather App</header>
+        <footer>
+          This app is coded by Lydia Prentice and is{" "}
+          <a
+            href="https://github.com/Lydia1596/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
