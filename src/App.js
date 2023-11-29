717d4dc5f77644106f3e3946859136a7ce35c9ce
Lydia1596 <lydiaprentice@hotmail.co.uk>
2023-11-29 07:30:58 +0000

    Import axios and create weather component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Weather App</header>
+      <Weather />
       <footer>
         This app is coded by Lydia Prentice and is{" "}
         <a
