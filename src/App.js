b4f444b49a08c9ae9374c8c120cd52b34234ec75
Lydia1596 <lydiaprentice@hotmail.co.uk>
2023-12-02 09:13:05 +0000

    Created HTML and CSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Weather App</header>
       <Weather />
       <footer>
         This app is coded by Lydia Prentice and is{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,53 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  return <h1>Hello from weather</h1>;
+  return (
+    <div className="container">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="search-bar w-100"
+            />
+          </div>{" "}
+          <div className="col-3">
+            <input
+              type="submit"
+              className="btn btn-primary w-100"
+              value="Search"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li className="date-time">Saturday 08:02</li>
+        <li className="conditions">Mist</li>
+      </ul>
+      <div className="row">
+        <div className="col">
+          <div className="clearfix">
+            <div className="float-left">
+              <img
+                src="https://openweathermap.org/img/wn/10d@2x.png"
+                alt="description"
+                className="weather-icon"
+              />
+              <span className="temperature">8</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <ul className="weather-description">
+            <li>Humidity: 88%</li>
+            <li>Wind: 4.92km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
